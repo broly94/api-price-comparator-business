682422843d4aadbe85e11eabffe101de0638642a
Leonel <joaquin_leonel_carro@hotmail.com>
2025-10-27 11:52:52 -0300

    fix: ConfigModule para producción en Docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,10 @@ import { CatalogProcessingModule } from './modules/catalog-processing/catalog-pr
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: '.env.development',
+      envFilePath:
+        process.env.NODE_ENV === 'production' ? undefined : '.env.development',
+      // O, de forma más simple, permite que las variables del sistema anulen los archivos .env
+      ignoreEnvFile: process.env.NODE_ENV === 'production',
     }),
     CatalogProcessingModule,
   ],
